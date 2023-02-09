89f314385c9354f0a6a4c65f98d27b17fe3478e3
Akashsawai <akashsawai9@gmail.com>
2023-02-09 13:15:41 +0530

    m

--- session1/provider.tf
+++ session1/provider.tf
@@ -1,10 +1,5 @@
 provider "aws" {
-  region     = "us-west-2"
+  region     = "ap-south-1"
   access_key = "AKIAV3HCXEBOWKTOANOK"
   secret_key = "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"
-}
-
-provider "hashicorp/first" {
-  version  = "1.0.0"
-  registry = "registry.terraform.io"
 }
