4df40eb8503d3dc6895b1fdedd80682c03831756
Akashsawai <akashsawai9@gmail.com>
2023-02-09 13:52:15 +0530

    g

--- session1/provider.tf
+++ session1/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region    = "ap-south-1"
-  access_key = "AKIAV3HCXEBOSSS5JBVY"
-  secret_key = "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"
+  region    = "us-east-1"
+  access_key = "AKIAV3HCXEBOQNEBSUMM"
+  secret_key = "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"
 }
