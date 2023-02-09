b00d5fb1a02856b93669a0bd246096cb405d373a
Akashsawai <akashsawai9@gmail.com>
2023-02-09 14:02:42 +0530

    b

--- session1/main.tf
+++ session1/main.tf
@@ -1,8 +1,17 @@
-resource "aws_iam_user" "lb" {
-  name = "user1"
-  path = "/system/"
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
 
-  tags = {
-    tag-key = "tag-value"
+  name = "single-instance"
+
+  ami                    = "ami-00874d747dde814fa"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0963905d9da30a633"]
+
+   tags = {
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
